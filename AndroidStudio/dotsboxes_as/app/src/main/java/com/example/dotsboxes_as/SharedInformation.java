f4aaacf3782e4cc5a23d16a1a68f42c423a5bbd4
Rina <inserttheemailyouusehere@gmail.com>
2022-04-20 22:29:16 -0400

    Tell app to use the right colors and icons for everything

--- AndroidStudio/dotsboxes_as/app/src/main/java/com/example/dotsboxes_as/SharedInformation.java
+++ AndroidStudio/dotsboxes_as/app/src/main/java/com/example/dotsboxes_as/SharedInformation.java
@@ -20,8 +20,20 @@ public class SharedInformation {
 //    private final ArrayList<IconColor> iconColorMappings = new ArrayList();
 
 
-    private final IconColor[] iconColorMappings = { new IconColor(R.drawable.frog, Color.GREEN),
-                                                    new IconColor(R.drawable.cat, Color.YELLOW) };
+    private final IconColor[] iconColorMappings = {
+            new IconColor(R.drawable.bunny, Color.parseColor("#ff75bc")),
+            new IconColor(R.drawable.cat, Color.parseColor("#ffca06")),
+            new IconColor(R.drawable.bird, Color.parseColor("#409e75")),
+            new IconColor(R.drawable.deer, Color.parseColor("#f8b0be")),
+            new IconColor(R.drawable.frog, Color.parseColor("#ff9d31")),
+            new IconColor(R.drawable.owl, Color.parseColor("#f20730")),
+            new IconColor(R.drawable.fish, Color.parseColor("#1f54ff")),
+            new IconColor(R.drawable.bat, Color.parseColor("#b346cf")),
+            new IconColor(R.drawable.swan, Color.parseColor("#9b1f36")),
+            new IconColor(R.drawable.penguin, Color.parseColor("#f6917d")),
+            new IconColor(R.drawable.butterfly, Color.parseColor("#44bba0")),
+            new IconColor(R.drawable.wolf, Color.parseColor("#a5abc2"))
+    };
 
 //    public void setIconColorMappings(){
 //        iconColorMappings.add(new IconColor(R.drawable.frog, Color.GREEN));
